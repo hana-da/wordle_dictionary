4cb1711f9b5b635e7456666e7038952dcd46271e
hana-da <hana-da@users.noreply.github.com>
2022-02-14 12:02:59 +0900

    Implement WordleLookupController

--- wordle_lookup/config/routes.rb
+++ wordle_lookup/config/routes.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'wordle_lookup/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'wordle_lookup#index'
 end
